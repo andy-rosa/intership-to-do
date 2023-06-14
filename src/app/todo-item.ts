bb76101173cdd299919f8e50ef10338c510e96d1
andy-rosa <andy.roshitu@gmail.com>
2023-06-14 23:30:16 +0300

    add: todo-item class

---
+++ src/app/todo-item.ts
@@ -0,0 +1,25 @@
+import {TodoStatus} from "./types/todo-status";
+
+export class TodoItem {
+  private _id: number;
+  public title: string;
+  private _status: TodoStatus;
+
+  constructor(id: number, title: string, status: TodoStatus) {
+    this._id = id;
+    this.title = title;
+    this._status = status;
+  }
+
+  get id(): number {
+    return this._id;
+  }
+
+  get status(): TodoStatus {
+    return this._status;
+  }
+
+  set status(value: TodoStatus) {
+    this._status = value;
+  }
+}
---
+++ src/app/types/todo-status.ts
@@ -0,0 +1,5 @@
+export enum TodoStatus {
+  Normal = 'normal',
+  Important = 'important',
+  Completed = 'completed'
+}
